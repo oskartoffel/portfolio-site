69d99c9aaf45eaf8dcf8cfb1e64e69670763132b
oskartoffel <oskar.wasmer@gmail.com>
2025-03-03 12:09:03 +0100

    In the middle of something, aber die arbeit ruft:(((+1

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-// Import other pages as you create them
+import CV from './pages/CV';
+import CoverLetter from './pages/CoverLetter';
+import Portfolio from './pages/Portfolio';
+import EcosystemSimulation from './pages/EcosystemSimulation';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* Add more routes here as you create more pages */}
+        <Route path="/cv" element={<CV />} />
+        <Route path="/cover-letter" element={<CoverLetter />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/ecosystem-simulation" element={<EcosystemSimulation />} />
+        {/* Add more routes as needed */}
       </Routes>
     </Router>
   );
